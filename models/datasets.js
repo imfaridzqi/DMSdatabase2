7a9bbec12ea4833ce85b3c3e9719ec3dd0980a66
imfaridzqi <imfaridzqi@gmail.com>
2022-06-27 16:55:29 +0700

    fixing timestamps

--- models/datasets.js
+++ models/datasets.js
@@ -7,53 +7,56 @@ let date = dateObj.getDate();
 let year = dateObj.getFullYear();
 let dateNow = year + "-" + month + "-" + date;
 
-const datasetsSchema = new Schema({
-  nama: {
-    type: String,
+const datasetsSchema = new Schema(
+  {
+    nama: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    handphone: {
+      type: String,
+    },
+    perusahaan: {
+      type: String,
+    },
+    jabatan: {
+      type: String,
+    },
+    ttl: {
+      type: Date,
+    },
+    alamat: {
+      type: String,
+    },
+    status: {
+      type: String,
+      lowercase: true,
+      enum: ["closing", "rapot", "schedule", "refollow up", "program"],
+    },
+    program: {
+      type: String,
+      lowercase: true,
+      enum: ["ssm", "sdm", "subsidism", "subsididm"],
+    },
+    keterangan: {
+      type: String,
+    },
+    batch: {
+      type: String,
+    },
+    // tglInput: {
+    //   type: Date,
+    //   default: dateNow,
+    // },
+    sumber: {
+      type: String,
+      lowercase: true,
+      enum: ["kontak", "whatsapp", "wordpress"],
+    },
   },
-  email: {
-    type: String,
-  },
-  handphone: {
-    type: String,
-  },
-  perusahaan: {
-    type: String,
-  },
-  jabatan: {
-    type: String,
-  },
-  ttl: {
-    type: Date,
-  },
-  alamat: {
-    type: String,
-  },
-  status: {
-    type: String,
-    lowercase: true,
-    enum: ["closing", "rapot", "schedule", "refollow up", "program"],
-  },
-  program: {
-    type: String,
-    lowercase: true,
-    enum: ["ssm", "sdm", "subsidism", "subsididm"],
-  },
-  keterangan: {
-    type: String,
-  },
-  batch: {
-    type: String,
-  },
-  tglInput: {
-    type: Date,
-    default: dateNow,
-  },
-  sumber: {
-    type: String,
-    lowercase: true,
-    enum: ["kontak", "whatsapp", "wordpress"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Datasets", datasetsSchema);
